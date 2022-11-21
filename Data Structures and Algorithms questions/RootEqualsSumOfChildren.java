c439281ca7cbf546aee145f1f840ef5c69fa1a1e
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2022-11-21 08:51:09 +0300

    Add files via upload

---
+++ Data Structures and Algorithms questions/RootEqualsSumOfChildren.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //function which checks whether the value of root node is equal to the sum of the value of left node and the value of right node 
+    public boolean checkTree(TreeNode root) {
+        return (root.val == root.left.val + root.right.val);
+        
+    }
+
+/* You are given the root of a binary tree that consists of exactly 3 nodes: the root, its left child, and its right child.
+
+Return true if the value of the root is equal to the sum of the values of its two children, or false otherwise. */
+
+}
