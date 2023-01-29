b61c952a5adab44b2a61a60a61a523d954902130
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2023-01-29 16:02:27 +0300

    Add files via upload

---
+++ Leetcode_Java_Part2/FindThePivotInteger.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int leftSummation = 0;
+        int rightSummation = n * (n + 1) / 2;
+        for (int a = 1; a <= n; a++) {
+            rightSummation = rightSummation - a;
+            if (leftSummation == rightSummation) {
+                return a;
+            }
+            leftSummation = leftSummation + a;
+        }
+        return -1;
+    }
+}
+
+// Given a positive integer n, find the pivot integer x such that:
+
+// // The sum of all elements between 1 and x inclusively equals the sum of /// all elements between x and n inclusively.
+// Return the pivot integer x. If no such integer exists, return -1. It is //// guaranteed that there will be at most one pivot index for the given input.
+
+ 
+
+// Example 1:
+
+// Input: n = 8
+// Output: 6
+// Explanation: 6 is the pivot integer since: 1 + 2 + 3 + 4 + 5 + 6 = 6 + 7 + 8 = 21.
+
+// Example 2:
+
+// Input: n = 1
+// Output: 1
+// Explanation: 1 is the pivot integer since: 1 = 1.
+
+// Example 3:
+
+// Input: n = 4
+// Output: -1
+// Explanation: It can be proved that no such integer exist.
