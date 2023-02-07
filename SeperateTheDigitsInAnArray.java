b6a4084041cba3f7eab69744434095a733de3af5
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2023-02-08 00:27:06 +0300

    Add files via upload
    Separate the digits in an array

---
+++ SeperateTheDigitsInAnArray.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer>digitsOfNums = new ArrayList<Integer>();
+        String str = "";
+        for(int i = 0 ; i < nums.length; i++) {
+            str = str + nums[i];
+        }
+
+        for(int j = 0 ; j < str.length(); j++) {
+            char ch = str.charAt(j);
+            String string = String.valueOf(ch);
+            Integer str_int = Integer.valueOf(string);
+            digitsOfNums.add(str_int);
+        }
+
+        int[] returnedDigits = digitsOfNums.stream().mapToInt(Integer::intValue).toArray();
+        return returnedDigits;
+    }
+}
+
+//Given an array of positive integers nums, return an array answer that consists of the digits of //each integer in nums after separating them in the same order they appear in nums.
+
+To separate the digits of an integer is to get all the digits it has in the same order.
+
+For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+ 
+
+Example 1:
+
+Input: nums = [13,25,83,77]
+Output: [1,3,2,5,8,3,7,7]
+Explanation: 
+- The separation of 13 is [1,3].
+- The separation of 25 is [2,5].
+- The separation of 83 is [8,3].
+- The separation of 77 is [7,7].
+answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+Example 2:
+
+Input: nums = [7,1,3,9]
+Output: [7,1,3,9]
+Explanation: The separation of each integer in nums is itself.
+answer = [7,1,3,9].
+
