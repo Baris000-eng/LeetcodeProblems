1143ec7917e70805ecad3aac83cec1b639688da9
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2023-02-12 12:36:32 +0300

    Update LongerContigiousSegmentOfOnesThanZeros.java

--- Leetcode_Java_Part3/LongerContigiousSegmentOfOnesThanZeros.java
+++ Leetcode_Java_Part3/LongerContigiousSegmentOfOnesThanZeros.java
@@ -27,34 +27,37 @@ class Solution {
     }
 }
 
-Given a binary string s, return true if the longest contiguous segment of 1's is strictly longer than the longest contiguous segment of 0's in s, or return false otherwise.
+//Given a binary string s, return true if the longest contiguous segment of 1's is strictly longer than the longest contiguous segment of 0's in s, or return false otherwise.
 
-For example, in s = "110100010" the longest continuous segment of 1s has length 2, and the longest continuous segment of 0s has length 3.
-Note that if there are no 0's, then the longest continuous segment of 0's is considered to have a length 0. The same applies if there is no 1's.
+//For example, in s = "110100010" the longest continuous segment of 1s has length 2, and the longest continuous segment of 0s has length 3.
+//Note that if there are no 0's, then the longest continuous segment of 0's is considered to have a length 0. The same applies if there is no 1's.
 
  
 
-Example 1:
+//Example 1:
 
-Input: s = "1101"
-Output: true
-Explanation:
-The longest contiguous segment of 1s has length 2: "1101"
-The longest contiguous segment of 0s has length 1: "1101"
-The segment of 1s is longer, so return true.
-Example 2:
+//Input: s = "1101"
+//Output: true
+//Explanation:
+//The longest contiguous segment of 1s has length 2: "1101"
+//The longest contiguous segment of 0s has length 1: "1101"
+//The segment of 1s is longer, so return true.
+    
+//Example 2:
 
-Input: s = "111000"
-Output: false
-Explanation:
-The longest contiguous segment of 1s has length 3: "111000"
-The longest contiguous segment of 0s has length 3: "111000"
-The segment of 1s is not longer, so return false.
-Example 3:
+//Input: s = "111000"
+//Output: false
+//Explanation:
+//The longest contiguous segment of 1s has length 3: "111000"
+//The longest contiguous segment of 0s has length 3: "111000"
+//The segment of 1s is not longer, so return false.
 
-Input: s = "110100010"
-Output: false
-Explanation:
-The longest contiguous segment of 1s has length 2: "110100010"
-The longest contiguous segment of 0s has length 3: "110100010"
-The segment of 1s is not longer, so return false.
+    
+//Example 3:
+
+//Input: s = "110100010"
+//Output: false
+//Explanation:
+//The longest contiguous segment of 1s has length 2: "110100010"
+//The longest contiguous segment of 0s has length 3: "110100010"
+//The segment of 1s is not longer, so return false.
