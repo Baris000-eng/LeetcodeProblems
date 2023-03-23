8dd259dab91c1e478109a8f628cb38d1f42dbcd3
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2023-03-23 10:53:48 +0300

    Add files via upload

---
+++ Leetcode_Java_Part3/CountTheNumberOfVowelStringsInRange.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int vowelStrings(String[] words, int left, int right) {
+        int length = words.length, vowelStrCount = 0;
+        for(int j = 0 ; j < length; j++) {
+            if(left <= j && j <= right) {
+                String str = words[j];
+                char chFirst = str.charAt(0);
+                char chLast = str.charAt(str.length() - 1);
+                if(isVowel(chFirst) && isVowel(chLast)) {
+                    vowelStrCount++;
+                }
+                
+            }
+        }
+
+        return vowelStrCount;
+        
+    }
+
+
+    public boolean isVowel(char ch) {
+        if(ch=='a'||ch=='e'||ch=='ı'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='ö'||ch=='Ö') {
+            return true;
+        }
+
+        return false;
+
+    }
+}
+
+
+/*You are given a 0-indexed array of string words and two integers left and right.
+
+A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+
+ 
+
+Example 1:
+
+Input: words = ["are","amy","u"], left = 0, right = 2
+Output: 2
+Explanation: 
+- "are" is a vowel string because it starts with 'a' and ends with 'e'.
+- "amy" is not a vowel string because it does not end with a vowel.
+- "u" is a vowel string because it starts with 'u' and ends with 'u'.
+The number of vowel strings in the mentioned range is 2.
+Example 2:
+
+Input: words = ["hey","aeo","mu","ooo","artro"], left = 1, right = 4
+Output: 3
+Explanation: 
+- "aeo" is a vowel string because it starts with 'a' and ends with 'o'.
+- "mu" is not a vowel string because it does not start with a vowel.
+- "ooo" is a vowel string because it starts with 'o' and ends with 'o'.
+- "artro" is a vowel string because it starts with 'a' and ends with 'o'.
+The number of vowel strings in the mentioned range is 3.
+ 
+
+Constraints:
+
+1 <= words.length <= 1000
+1 <= words[i].length <= 10
+words[i] consists of only lowercase English letters.
+0 <= left <= right < words.length*/
