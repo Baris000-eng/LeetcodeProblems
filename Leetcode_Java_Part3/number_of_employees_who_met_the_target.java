6072f77409c4f3e0bfb668a2e608300c29fd4af2
Barış Kaplan <63741832+Baris000-eng@users.noreply.github.com>
2023-08-12 13:34:22 +0300

    Create number_of_employees_who_met_the_target.java

---
+++ Leetcode_Java_Part3/number_of_employees_who_met_the_target.java
@@ -0,0 +1,20 @@
+/* There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
+
+The company requires each employee to work for at least target hours.
+
+You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+
+Return the integer denoting the number of employees who worked at least target hours. */
+
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int numOfEmployeesMeetingTheTarget = 0;
+        for(int hour: hours) {
+            if(hour >= target) {
+                numOfEmployeesMeetingTheTarget += 1;
+            }
+        }
+
+        return numOfEmployeesMeetingTheTarget;
+    }
+}
